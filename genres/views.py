f2b4f18efffa9d424363ae3ff71372b43daedcf2
eriknathan <eriknathan.contato@gmail.com>
2025-07-02 10:30:53 -0300

    feat: update and delete crud

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,12 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 
 @csrf_exempt
-def genre_view(request):
+def genre_creat_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
@@ -18,4 +19,29 @@ def genre_view(request):
         return JsonResponse(
             {'id': new_genre.id, 'name': new_genre.name},
             status=201,
+        )
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+
+    if request.method == 'GET':
+        data = [{'id': genre.id, 'name': genre.name}]
+        return JsonResponse(data, safe=False)
+
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse(
+            {'id': genre.id, 'name': genre.name},
+            status=201,
+        )
+
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse(
+            {'message': 'Gênero excluído com sucesso!'},
+            status=204,
         )

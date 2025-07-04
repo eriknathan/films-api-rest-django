52b4409597f5a2d40f643355a17a191191486b3a
eriknathan <eriknathan.contato@gmail.com>
2025-07-04 11:41:48 -0300

    refator: serializers in crud

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_creat_list_view, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('genres/', genre_creat_list_view, name='genre-create-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(),
+         name='genre-detail-view'),
 ]

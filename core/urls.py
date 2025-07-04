e46c1dba3e157f5773e17785efaafffedc2d9dd8
eriknathan <eriknathan.contato@gmail.com>
2025-07-04 08:41:53 -0300

    feat: updates

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from genres.views import genre_creat_list_view, genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres/', genre_creat_list_view, name='genre-create-list'),
     path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]

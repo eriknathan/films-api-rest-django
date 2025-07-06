e3563384fe06e6efd688cd4ed833cdf72a0886d4
eriknathan <eriknathan.contato@gmail.com>
2025-07-06 08:25:47 -0300

    feat: app updates

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetriveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetriveUpdateDestroyView
 
 
 urlpatterns = [
@@ -14,4 +15,8 @@ urlpatterns = [
     path('actors/', ActorCreateListView.as_view(), name='actors-create-list'),
     path('actors/<int:pk>/', ActorRetriveUpdateDestroyView.as_view(),
          name='actors-detail-view'),
+
+    path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
+    path('movies/<int:pk>/', MovieRetriveUpdateDestroyView.as_view(),
+         name='movies-detail-view'),
 ]

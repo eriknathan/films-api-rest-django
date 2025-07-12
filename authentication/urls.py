8ea47b9640f69831d4fb77c687c31c17d139736f
eriknathan <eriknathan.contato@gmail.com>
2025-07-11 21:06:29 -0300

    feat: authetication

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path('authentication/token/', TokenObtainPairView.as_view(), 
+    path(
+        'authentication/token/', TokenObtainPairView.as_view(),
         name='token_obtain_pair'),
 ]

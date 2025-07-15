be9d96916f5b558235bdbe95b902422d14da0fd7
eriknathan <eriknathan.contato@gmail.com>
2025-07-15 15:38:19 -0300

    feat: adding permissions in groups and users

--- genres/views.py
+++ genres/views.py
@@ -1,16 +1,17 @@
 from genres.models import Genre
 from rest_framework import generics
+from core.permissions import GlobalDefaultPermission
 from rest_framework.permissions import IsAuthenticated
 from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)    
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)    
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
